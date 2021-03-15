154f8556aa894cacd0e6c448b1570ed51e0aa79f
Allison Cheng <alli@Allisons-Air.fios-router.home>
2021-03-14 21:03:23 -0400

    added updateUserInfo controller

--- AOITServer6/src/main/java/AOITServer/Controllers/UserController.java
+++ AOITServer6/src/main/java/AOITServer/Controllers/UserController.java
@@ -15,19 +15,33 @@ import java.sql.SQLIntegrityConstraintViolationException;
 public class UserController {
     private PreparedStatement psCreateUser;
     private PreparedStatement psLogin;
+    private PreparedStatement psUpdateUserInfo;
+
     public UserController(int connectionIndex,DatabaseSingleton ds){
-        String sql1 = String.format("INSERT INTO %s (%s,%s,%s) VALUES (?,?,?)",
+        String sqlCreateUser = String.format("INSERT INTO %s (%s,%s,%s) VALUES (?,?,?)",
                 AOITUsersTable.TABLENAME, AOITUsersTable.USERNAME, AOITUsersTable.PASSWORD
                 , AOITUsersTable.ROLE);
 
-        String sql2 = String.format("SELECT %s FROM %s WHERE %s = ? AND %s = ?",AOITUsersTable.ROLE,
+        String sqlLogin = String.format("SELECT %s FROM %s WHERE %s = ? AND %s = ?",AOITUsersTable.ROLE,
                 AOITUsersTable.TABLENAME,AOITUsersTable.USERNAME,AOITUsersTable.PASSWORD);
 
-        int index1 = ds.createPreparedStatement(connectionIndex,sql1);
-        int index2 = ds.createPreparedStatement(connectionIndex,sql2);
+        String sqlUpdateUserInfo = String.format(
+                "UPDATE %s SET %s = ?,  %s = ?,  %s = ?,  %s = ?,  %s = ? WHERE %s = ?",
+                AOITUsersTable.TABLENAME,
+                AOITUsersTable.NAME,
+                AOITUsersTable.ADDRESS,
+                AOITUsersTable.BIRTHDAY,
+                AOITUsersTable.EMAIL,
+                AOITUsersTable.PHONENUMBER,
+                AOITUsersTable.USERNAME);
+
+        int index1 = ds.createPreparedStatement(connectionIndex,sqlCreateUser);
+        int index2 = ds.createPreparedStatement(connectionIndex,sqlLogin);
+        int index3 = ds.createPreparedStatement(connectionIndex,sqlUpdateUserInfo);
 
         psCreateUser = ds.getPreparedStatement(index1);
         psLogin = ds.getPreparedStatement(index2);
+        psUpdateUserInfo = ds.getPreparedStatement(index3);
 
     }
     public Handler createUser(){
@@ -100,5 +114,36 @@ public class UserController {
         };
     }
 
+    public Handler updateUserInfo(){
+        return ctx ->{
+            String username = ctx.queryParam("Username");
+            String name = ctx.queryParam("Name");
+            String address = ctx.queryParam("Address");
+            String birthday = ctx.queryParam("Birthday");
+            String email = ctx.queryParam("Email");
+            String phonenumber = ctx.queryParam("Phone");
 
+            if(username == null){
+                ctx.status(404).json(new ErrorJson(false,"Query parameter \"Username\" not found"));
+            }
+            else if(name == null){
+                ctx.status(404).json(new ErrorJson(false,"Query parameter \"Name\" not found"));
+            }
+            else{
+                psUpdateUserInfo.setString(1,name);
+                psUpdateUserInfo.setString(2,address);
+                psUpdateUserInfo.setString(3,birthday);
+                psUpdateUserInfo.setString(4,email);
+                psUpdateUserInfo.setString(5,phonenumber);
+                psUpdateUserInfo.setString(6,username);
+
+                try {
+                    psUpdateUserInfo.execute();
+                }catch(SQLException e){
+                    System.out.println(e);
+                    ctx.status(404).json(new ErrorJson(false,"Could not update User Info"));
+                }
+            }
+        };
+    }
 }
